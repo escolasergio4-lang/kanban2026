95dca61f1055f10cdf6f3e5ff7ee177a3022545d
escolasergio4-lang <escolasergio4@gmail.com>
2026-01-13 22:22:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,26 @@
-var CACHE_NAME = 'nullboard-educacional-v1';
-var urlsToCache = [
+const CACHE_NAME = 'planejamento-educacional-v3.3-offline';
+const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './icon.png',
-  './extras/Barlow-Regular.woff',
-  './extras/Barlow-Medium.woff',
-  './extras/IBMPlexSans-Regular.woff',
-  './extras/IBMPlexSans-Medium.woff',
-  './extras/OpenSans-Regular.woff',
-  './extras/OpenSans-600.woff',
-  './extras/MavenPro-Regular.woff',
-  './extras/MavenPro-500.woff',
-  './extras/favicon-16.png'
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.0/Sortable.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
   );
 });
